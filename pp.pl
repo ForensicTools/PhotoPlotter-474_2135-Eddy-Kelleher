8a6afb938966e4075ee4db392c6e61b39bbc2a82
jfk2670 <jackkelleher1993@gmail.com>
2014-04-19 10:32:36 -0400

    Update pp.pl

--- pp.pl
+++ pp.pl
@@ -53,31 +53,59 @@ foreach my $name (@filenames)
     my $file = shift;
 
     #check to see if the file exists
-    if (-e $filename)
+    if ( -e $filename )
     {
     	my $exif = Image::ExifTool->new()
     	
-    	if($exif = ExtractInfo($file);
+    	if( $exif = ExtractInfo($file );
     }
 }
 
 # If html AND we actually have files
-if ( ($htmloutput) && (keys(%file_listing) > 0) )
+if ( ( $htmloutput ) && (keys( %file_listing ) > 0 ) )
 {    
-    #timestamped output filename
-    my $htmloutputfile = "pp-output-".time.".html";
+    	#timestamped output filename
+	my $htmloutputfile = "pp-output-".time.".html";
 
-    open(my $html_output_file, ">".$htmloutputfile) || die("Unable to open $htmloutputfile for writing\n");
+    	open( my $html_output_file, ">".$htmloutputfile ) || die( "Unable to open $htmloutputfile for writing\n" );
 
-    my $htmltable = HTML::QuickTable->new(border => 1, labels => 1);
+	#create HTML table
+	my $htmltable = HTML::QuickTable->new( border => 1, labels => 1 );
 
-    # Added preceding "/" to "Filename" so that the HTML::QuickTable sorting doesn't result in
-    # the column headings being re-ordered after / below a filename beginning with a "\". 
-    $file_listing{"/Filename"} = "GoogleMaps Link";
-
-    print $html_output_file "<HTML>";
-    print $html_output_file $htmltable->render(\%file_listing);
-    print $html_output_file "<\/HTML>";
-
-    close($htmloutputfile);
+	# Added preceding "/" to "Filename" so that the HTML::QuickTable sorting doesn't result in
+	# the column headings being re-ordered after / below a filename beginning with a "\". 
+	$file_listing{ "/Filename" } = "GoogleMaps Link";
+	
+	print $html_output_file "<HTML>"
+	print $html_output_file $htmltable->render( \%file_listing );
+	print $html_output_file "<\/HTML>";
+	
+	close( $htmloutputfile );
 }
+
+ub ProcessFilename
+{
+	my $filename = shift;
+	
+	if (-e $filename) #file must exist
+	{
+	 	my $exif = Image::ExifTool->new();
+	        # Extract all info from existing image
+	        if ($exif->ExtractInfo($filename))
+	        {
+	            # Ensure all 4 GPS params are present 
+	            # ie GPSLatitude, GPSLatitudeRef, GPSLongitude, GPSLongitudeRef
+	            # The Ref values indicate North/South and East/West
+	            if ($exif->HasLocation()) 
+	            {
+	              	my ($lat, $lon) = $exif->GetLocation(); 
+	                
+	                print("\n$filename contains Lat: $lat, Long: $lon\n");
+	                print("URL: http://maps.google.com/maps?q=$lat,+$lon($filename)&iwloc=A&hl=en\n");
+	                
+	                if ($htmloutput) # save GoogleMaps URL to global hashmap indexed by filename
+	                {
+	                    $file_listing{$filename} = "<A HREF = \"http://maps.google.com/maps?q=$lat,+$lon($filename)&iwloc=A&hl=en\"> http://maps.google.com/maps?q=$lat,+$lon($filename)&iwloc=A&hl=en</A>";
+	                }
+	                
+	                return 1;
